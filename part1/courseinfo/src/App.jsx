857a10d563e9cab0b4c08d7e41d06e602a4c0871
Yevhenii <eeevg27@gmail.com>
2025-06-04 21:20:47 +0300

    refactor Content

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,9 +4,19 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.ex1} />
+      <Part part={props.part2} exercise={props.ex2} />
+      <Part part={props.part3} exercise={props.ex3} />
+    </div>
   )
 }
 
@@ -31,9 +41,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1}/>
-      <Content part={part2} exercise={exercises2}/>
-      <Content part={part3} exercise={exercises3}/>
+      <Content 
+        part1={part1} ex1={exercises1} 
+        part2={part2} ex2={exercises2} 
+        part3={part3} ex3={exercises3}/>
       <Total exercises={[exercises1, exercises2, exercises3]}/>
     </div>
   )
