7054f991b5efaec22c41d7751f31b138fc141ce6
Yevhenii <eeevg27@gmail.com>
2025-06-16 17:28:57 +0300

    use state

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,60 +1,13 @@
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  };
-
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  var sum = 0;
-  for (var index = 0; index < props.parts.length; index++) {
-    sum = sum + props.parts[index].exercises;
-  }
-  return <p>Number of exercises: {sum} </p>;
-};
+import { useState } from "react";
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [counter, setCounter] = useState(0);
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  console.log("rendering...", counter);
+
+  return <div>{counter}</div>;
 };
 
 export default App;
--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx'
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(<App />)
+let counter = 0;
+
+createRoot(document.getElementById("root")).render(<App counter={counter} />);
