690d70f72f7c66bc7d11531e17b6262aa5ac7c38
Yevhenii <eeevg27@gmail.com>
2025-07-02 15:04:57 +0300

    refactor - add separate components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from "react";
 
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  const handleClick = () => {
+    console.log("clicked");
+    setCounter(counter + 1);
+  };
 
-  console.log("rendering...", counter);
+  const resetCounter = () => {
+    setCounter(0);
+  };
 
-  return <div>{counter}</div>;
+  return (
+    <>
+      <div>
+        <Display counter={counter} />
+        <Button text="Increment" onClick={handleClick} />
+      </div>
+      <div>
+        <Button text="Reset" onClick={resetCounter} />
+      </div>
+    </>
+  );
 };
 
 export default App;
