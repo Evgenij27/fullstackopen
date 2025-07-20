889c2a445b7cb20af9a78dbb0c9c7bfae5c78de8
Yevhenii <eeevg27@gmail.com>
2025-07-20 14:56:59 +0300

    part 2: add note importance change handler

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -35,6 +35,16 @@ const App = () => {
     setNewNote(event.target.value);
   };
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((resp) => {
+      setNotes(notes.map((note) => (note.id === id ? resp.data : note)));
+    });
+  };
+
   return (
     <div>
       <h1>Notes</h1>
@@ -45,7 +55,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+            note={note}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
